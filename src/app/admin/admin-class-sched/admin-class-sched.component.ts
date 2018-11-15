a70e17bee3c8f4f165cdf922b3ba8ed6ca5acd12
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-11-15 21:38:03 +0530

    user delete ok

--- src/app/admin/admin-class-sched/admin-class-sched.component.ts
+++ src/app/admin/admin-class-sched/admin-class-sched.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { clzService} from '../../services/clz.service';
 
 @Component({
   selector: 'app-admin-class-sched',
   templateUrl: './admin-class-sched.component.html',
-  styleUrls: ['./admin-class-sched.component.scss']
+  styleUrls: ['./admin-class-sched.component.scss'],
+  providers: [clzService]
 })
 export class AdminClassSchedComponent implements OnInit {
 
-  constructor() { }
+  form5;
+
+  constructor(
+    private fb5: FormBuilder,
+    private getClzes: clzService
+  ) { 
+    this.form5 = this.fb5.group({
+      subjectName: ['', Validators.required],
+      hallNo: ['', Validators.required],
+      
+    })
+   }
 
   ngOnInit() {
   }
