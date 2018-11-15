d3057a4720add46d475996b06bb86c35b61d9159
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-11-16 01:37:48 +0530

    clz add and view details ok

--- src/app/admin/admin-class-sched/admin-class-sched.component.ts
+++ src/app/admin/admin-class-sched/admin-class-sched.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { clzService} from '../../services/clz.service';
+import { validateConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-admin-class-sched',
@@ -11,19 +12,66 @@ import { clzService} from '../../services/clz.service';
 export class AdminClassSchedComponent implements OnInit {
 
   form5;
+  classes: any[] = [];
 
   constructor(
     private fb5: FormBuilder,
-    private getClzes: clzService
+    private Clzes: clzService
   ) { 
     this.form5 = this.fb5.group({
       subjectName: ['', Validators.required],
       hallNo: ['', Validators.required],
-      
+      grade: ['', Validators.required],
+      batch: ['', Validators.required],
+      stream: ['', Validators.required],
+      time: ['', Validators.required]
     })
    }
 
   ngOnInit() {
+    this.getAllClzes()
   }
 
+  getAllClzes(){
+    this.Clzes.getAllClzes()
+      .subscribe(result => {
+        this.classes = result.json().Clz;
+        // console.log(this.classes);
+        if(result.json()) console.log(result.json());
+      })
+  }
+
+  onSubmit(form5){
+    console.log(form5.value);
+    this.Clzes.createClz(form5.value)
+      .subscribe(result => {
+        if(result.json().state) alert("Class Created Successfully");
+        else alert("Something Went Wrong");
+        console.log(result);
+      })
+    this.form5.reset();
+  }
+
+  deleteClz(clz){
+    console.log(clz._id);
+    let index = this.classes.indexOf(clz);
+    this.classes.splice(index, 1);
+    this.Clzes.deleteClz(clz._id)
+      .subscribe(response => {
+        if(response.json().state) alert("Class Deleted");
+        else alert("Something Went Wrong");
+      })
+  }
+
+  get subjectName(){ return this.form5.get('subjectName'); }
+
+  get hallNo(){ return this.form5.get('hallNo'); }
+
+  get grade(){ return this.form5.get('grade'); }
+
+  get batch(){ return this.form5.get('batch'); }
+
+  get stream(){ return this.form5.get('stream'); }
+
+  get time(){ return this.form5.get('time'); }
 }
--- src/app/services/clz.service.ts
+++ src/app/services/clz.service.ts
@@ -17,4 +17,12 @@ export class clzService{
         // return this.http.get('http://localhost:3000/clz/');
     }
 
+    createClz(clz){
+        return this.http.post('https://polar-meadow-28819.herokuapp.com/clz/', clz);
+    }
+
+    deleteClz(clzId){
+        return this.http.delete('https://polar-meadow-28819.herokuapp.com/clz/'+clzId);
+    }
+
 }
