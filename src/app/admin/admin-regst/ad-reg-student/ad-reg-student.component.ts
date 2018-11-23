f5551695faaa20e3205ac8f7b606033fc21a38f4
Dinusha Naveen <dldndasanayaka@gmail.com>
2018-11-23 23:50:23 +0530

    edits

--- src/app/admin/admin-regst/ad-reg-student/ad-reg-student.component.ts
+++ src/app/admin/admin-regst/ad-reg-student/ad-reg-student.component.ts
@@ -69,7 +69,7 @@ export class AdRegStudentComponent implements OnInit {
     console.log(form1.value);
     this.register.register(form1.value)
       .subscribe(result => {
-        if(result.json().state) alert("Student registered successfully"+"Index No: "+result.json().indexNo);
+        if(result.json().state) alert("Student Registered Successfully\n"+"Index No: "+result.json().indexNo);
         else if(result.json().exist){ 
           alert("Student already exist");
         } 
