9b70c48cdb94eb64fd5c6527380a5868416d3c14
Janitha <janitharasanga133@gmail.com>
2018-12-05 18:50:00 +0530

    login button done completely

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,9 +30,9 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    if(localStorage.getItem('token') !== null){
-      this.router.navigate(['admin/dashboard']);
-    }
+    // if(localStorage.getItem('token') !== null){
+    //   this.router.navigate(['admin/dashboard']);
+    // }
   }
 
   onSubmit(form){
