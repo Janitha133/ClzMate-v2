7ee0d6b62f4bc0f7f5a7a362bbb15e890f60ffc6
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-07 16:05:23 +0530

    get clz _id from class list

--- src/app/admin/admin-regst/ad-reg-student/ad-reg-student.component.ts
+++ src/app/admin/admin-regst/ad-reg-student/ad-reg-student.component.ts
@@ -15,6 +15,8 @@ import { UserService } from '../../../services/users.service';
 export class AdRegStudentComponent implements OnInit {
 
   form1;
+  newClass: String
+  classArray : String[] = [];
   classes: any[] = [];
   mobnumPattern = "^((\\+91-?)|0)?[0-9]{10}$";
 
@@ -41,7 +43,7 @@ export class AdRegStudentComponent implements OnInit {
       fullName: ['', Validators.required],
       batch:[''],
       school: ['', Validators.required],
-      clzes: new FormArray([]),
+      clzes: Array([]),
       stream: [''],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -82,7 +84,7 @@ export class AdRegStudentComponent implements OnInit {
 
   get fullName(){return this.form1.get('fullName');}
 
-  get firstName(){return this.form1.get('firstName');}
+  get firstName(){return this.form1.get('firstName');} 
 
   get lastName(){return this.form1.get('lastName');}
 
@@ -102,7 +104,7 @@ export class AdRegStudentComponent implements OnInit {
 
   get district(){return this.form1.get('district');}
 
-  get batch(){return this.form1.get('batch');}
+  get batch(){return this.form1.get('batch');} 
 
   get fatherName(){return this.form1.get('fatherName');}
 
@@ -114,21 +116,51 @@ export class AdRegStudentComponent implements OnInit {
 
   get clzes(){return this.form1.get('clzes')}
 
-  addClz(clz){
-    console.log(clz);
-    // this.clzes.push(clz);
-    // this.clzes.push(new FormControl(clz));
+  // addClz(clz){
+  //   console.log(clz.value);
+    
+  //   // this.clzes.push(clz.value);
+  //   // this.clzes.push(new FormControl(clz));
+  //   this.classArray.push(clz.value);
+  //   // this.clzes[this.clzes.length] = "ssdf";
+  //   // console.log(clz); 
+  //   // console.log(this.classes);
+  //   // clz._id = ''; 
+  // }
+
+  addclz(clz: HTMLInputElement){
+    
+    // this.topics.push(new FormControl(clz.value));
     // this.topics.push(clz.value);
-    // this.clzes[this.clzes.length] = "ssdf";
-    // console.log(clz);
-    // console.log(this.classes);
-    // clz._id = ''; 
-  }
+    // console.log(clz.value);
+    this.newClass = clz.value;
+    const noOfClasses = this.classes.length;
+    // console.log(this.newClass);
+    // console.log(noOfClasses);
+    // console.log(this.clzes.value[0]);
+    // console.log(this.classes[0].subjectName);
+    clz.value = ''; 
+    for(var i=0; i<noOfClasses; i++){
+      // console.log(this.newClass);
+      if(this.classes[i].subjectName == this.newClass){
+        console.log(this.newClass);
+        // console.log(this.classes[i]._id)
+        var classId = this.classes[i]._id;
+        console.log(classId);
+        this.classArray.push(classId); 
+        // console.log(clz.value); 
+        console.log(this.classArray);
+      } else{
+        // console.log("Not in the list");
+      }
+    }
+  } 
 
   removeClz(topic:FormControl){
     this.clzes.removeAt(this.clzes.controls.indexOf(topic));
     // this.topics = this.topics.filter(item => item !== topic);
     console.log(topic);
+    console.log(this.clzes.value);
   }
 
   consoleLog(){
