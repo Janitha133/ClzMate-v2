85e712ec383f84a85ef0be49b1fb43f668b6c381
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-01-22 23:22:32 +0530

    some edit

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/Clzmate-v2'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Clzmate-v2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
