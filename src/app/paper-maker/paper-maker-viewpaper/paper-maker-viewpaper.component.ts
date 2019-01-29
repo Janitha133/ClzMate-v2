7d3421a29f9f5a1db7c9f7ce8ddf868a7c03e201
Janitha <janitharasanga133@gmail.com>
2019-01-29 20:51:26 +0530

    ppr mkr

--- src/app/paper-maker/paper-maker-viewpaper/paper-maker-viewpaper.component.ts
+++ src/app/paper-maker/paper-maker-viewpaper/paper-maker-viewpaper.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { FormBuilder } from '@angular/forms';
+import { ClzService } from 'src/app/services/clz.service';
+import { PapersService } from 'src/app/services/papers.service';
 
 @Component({
   selector: 'app-paper-maker-viewpaper',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaperMakerViewpaperComponent implements OnInit {
 
-  constructor() { }
+  defaultClzId = "Class Id";
+  clzes: any[] = [];
+  papers: any[] = [];
+
+  selectedClz: [''];
+  selectedClzNo: any;
+
+  form12;
+
+  constructor(
+    private http: Http,
+    private fb12: FormBuilder,
+    private Clzes: ClzService,
+    private Papers: PapersService
+  ) {
+    this.getAllClzes();
+    this.getAllPapers();
+
+    this.form12 = this.fb12.group({
+      clzid: [''],
+    })
+  }
 
   ngOnInit() {
+  }
+
+  getAllClzes() {
+    this.Clzes.getAllClzes()
+      .subscribe(response => {
+        this.clzes = response.json().Clz;
+        console.log(this.clzes);
+      })
+  }
+
+  getAllPapers() {
+    this.Papers.getAllPapers()
+      .subscribe(response => {
+        this.papers = response.json().Papers;
+        console.log(this.papers);
+      })
   }
 
 }
